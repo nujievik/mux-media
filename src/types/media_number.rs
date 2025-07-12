278a6a929cf729973f641ab31170781e2e58e4ba
nujievik <nujievik@gmail.com>
2025-07-12 14:01:58 +0500

    replace MediaNumber platform-dependent code

--- src/types/media_number.rs
+++ src/types/media_number.rs
@@ -1,182 +1,117 @@
-use std::ffi::OsStr;
+use std::{ffi::OsStr, mem::take};
 
 #[derive(Clone)]
 pub struct MediaNumber {
-    raw: String,
-    len_bytes: usize,
+    len_raw_bytes: usize,
+    s_nums_borders: Vec<(String, usize, usize)>,
     idx: Option<usize>,
-    digits: Vec<String>,
-    borders: Vec<(usize, usize)>,
-}
-
-impl From<&OsStr> for MediaNumber {
-    fn from(value: &OsStr) -> Self {
-        let bytes = get_bytes(value);
-        let len_bytes = bytes.len();
-
-        let (digits, borders) = get_ascii_digits(bytes);
-
-        let raw = digits.get(0).cloned().unwrap_or(String::new());
-
-        Self {
-            raw,
-            len_bytes,
-            idx: None,
-            digits,
-            borders,
-        }
-    }
+    s: String,
 }
 
 impl MediaNumber {
     pub fn as_str(&self) -> &str {
-        &self.raw
+        &self.s
     }
 
     pub fn to_u64(&self) -> u64 {
-        self.raw.parse::<u64>().unwrap_or(0)
+        self.s.parse::<u64>().unwrap_or(0)
     }
 
     pub fn upd(&mut self, value: &OsStr) {
-        let bytes = get_bytes(value);
-        let len_bytes = bytes.len();
+        let bytes = value.as_encoded_bytes();
+        let len_raw_bytes = bytes.len();
 
-        if self.len_bytes == len_bytes {
-            match self.idx {
-                Some(idx) => {
-                    if let Some(&(start, end)) = self.borders.get(idx) {
-                        let slice = &bytes[start..end];
-                        if let Some(raw) = get_ascii_digit_from_slice(slice) {
-                            self.raw = raw;
-                            return;
-                        }
-                    }
-                }
-                None => {}
-            }
+        if self.len_raw_bytes != len_raw_bytes {
+            *self = Self::from(bytes);
+            return;
+        }
 
-            // fallback
-            let (digits, borders) = get_ascii_digits(bytes);
-            let (raw, idx) = self.get_raw_from_unmatched(&digits);
+        if let Some(s) = self
+            .idx
+            .and_then(|idx| self.s_nums_borders.get(idx))
+            .and_then(|&(_, start, end)| get_s_num(&bytes[start..end]))
+        {
+            self.s = s;
+            return;
+        }
 
-            self.raw = raw;
-            self.idx = idx;
-            self.digits = digits;
-            self.borders = borders;
-        } else {
-            let (digits, borders) = get_ascii_digits(bytes);
+        let s_nums_borders = s_nums_borders_from_bytes(bytes);
+        let (s, idx) = self.get_s_idx_from_mismatch_num(&s_nums_borders);
 
-            self.raw = digits.get(0).cloned().unwrap_or(String::new());
-            self.len_bytes = len_bytes;
-            self.idx = None;
-            self.digits = digits;
-            self.borders = borders;
+        self.s_nums_borders = s_nums_borders;
+        self.idx = idx;
+        self.s = s;
+    }
+
+    #[inline(always)]
+    fn get_s_idx_from_mismatch_num(
+        &self,
+        s_nums_borsers: &[(String, usize, usize)],
+    ) -> (String, Option<usize>) {
+        s_nums_borsers
+            .into_iter()
+            .enumerate()
+            .find(|(i, vals)| Some(*vals) != self.s_nums_borders.get(*i))
+            .map(|(i, vals)| (vals.0.to_string(), Some(i)))
+            .unwrap_or(("".to_string(), None))
+    }
+}
+
+impl From<&OsStr> for MediaNumber {
+    fn from(value: &OsStr) -> Self {
+        value.as_encoded_bytes().into()
+    }
+}
+
+impl From<&[u8]> for MediaNumber {
+    fn from(bytes: &[u8]) -> Self {
+        let s_nums_borders = s_nums_borders_from_bytes(bytes);
+
+        let s = s_nums_borders
+            .get(0)
+            .map(|(s, _, _)| s.to_string())
+            .unwrap_or_default();
+
+        Self {
+            len_raw_bytes: bytes.len(),
+            s_nums_borders,
+            idx: None,
+            s,
         }
     }
-
-    fn get_raw_from_unmatched(&self, digits: &[String]) -> (String, Option<usize>) {
-        for (i, digit) in digits.iter().enumerate() {
-            if self.digits.get(i) != Some(digit) {
-                return (digit.clone(), Some(i));
-            }
-        }
-        ("".to_string(), None)
-    }
 }
 
-#[cfg(unix)]
-fn get_bytes(os_str: &OsStr) -> &[u8] {
-    use std::os::unix::ffi::OsStrExt;
-    os_str.as_bytes()
-}
+#[inline(always)]
+fn s_nums_borders_from_bytes(bytes: &[u8]) -> Vec<(String, usize, usize)> {
+    let mut s_nums_borders = Vec::<(String, usize, usize)>::new();
 
-#[cfg(windows)]
-fn get_bytes(os_str: &OsStr) -> Vec<u16> {
-    use std::os::windows::ffi::OsStrExt;
-    os_str.encode_wide().collect()
-}
-
-#[cfg(unix)]
-fn get_ascii_digit_from_slice(slice: &[u8]) -> Option<String> {
-    if slice.iter().all(|&c| c.is_ascii_digit()) {
-        Some(unsafe { String::from_utf8_unchecked(slice.to_vec()) })
-    } else {
-        None
-    }
-}
-
-#[cfg(windows)]
-fn get_ascii_digit_from_slice(slice: &[u16]) -> Option<String> {
-    if slice.iter().all(|&c| (0x30..=0x39).contains(&c)) {
-        let s: String = slice
-            .iter()
-            .map(|&c| char::from_u32(c as u32).unwrap())
-            .collect();
-        Some(s)
-    } else {
-        None
-    }
-}
-
-#[cfg(unix)]
-fn get_ascii_digits(bytes: &[u8]) -> (Vec<String>, Vec<(usize, usize)>) {
-    let mut digits = Vec::new();
-    let mut borders = Vec::new();
-
-    let mut current = Vec::new();
+    let mut current = Vec::<u8>::new();
     let mut start = 0;
-    let mut count = 0;
 
-    for &b in bytes {
-        if b.is_ascii_digit() {
-            current.push(b);
+    bytes.into_iter().enumerate().for_each(|(i, byte)| {
+        if byte.is_ascii_digit() {
+            current.push(*byte);
             if start == 0 {
-                start = count;
+                start = i;
             }
         } else if !current.is_empty() {
-            digits.push(unsafe { String::from_utf8_unchecked(current.clone()) });
-            current.clear();
-            borders.push((start, count + 1));
+            // Safely: only `is_ascii_digit()` bytes
+            let s = unsafe { String::from_utf8_unchecked(take(&mut current)) };
+            s_nums_borders.push((s, start, i));
             start = 0;
         }
-        count += 1;
-    }
+    });
 
-    if !current.is_empty() {
-        digits.push(unsafe { String::from_utf8_unchecked(current) });
-        borders.push((start, count));
-    }
-
-    (digits, borders)
+    s_nums_borders
 }
 
-#[cfg(windows)]
-fn get_ascii_digits(utf16: Vec<u16>) -> (Vec<String>, Vec<(usize, usize)>) {
-    let mut digits = Vec::new();
-    let mut borders = Vec::new();
-
-    let mut current = Vec::new();
-    let mut start = 0;
-    let mut count = 0;
-
-    for u in utf16 {
-        if (u as u8).is_ascii_digit() && u <= 0x7F {
-            current.push(u as u8);
-            if start == 0 {
-                start = count;
-            }
-        } else if !current.is_empty() {
-            digits.push(unsafe { String::from_utf8_unchecked(current.clone()) });
-            current.clear();
-            borders.push((start, count + 1));
-        }
-        count += 1;
-    }
-
-    if !current.is_empty() {
-        digits.push(unsafe { String::from_utf8_unchecked(current) });
-        borders.push((start, count));
-    }
-
-    (digits, borders)
+#[inline(always)]
+fn get_s_num(bytes: &[u8]) -> Option<String> {
+    bytes
+        .into_iter()
+        .all(|byte| byte.is_ascii_digit())
+        .then(|| {
+            // Safely: only `is_ascii_digit()` bytes
+            unsafe { String::from_utf8_unchecked(bytes.to_vec()) }
+        })
 }
