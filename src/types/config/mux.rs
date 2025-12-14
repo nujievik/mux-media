25b2d59e65e921ebe78f182a1c3872b63e0d7a60
Ivan Reutov <nujievik@gmail.com>
2025-12-14 13:56:18 +0500

    Replace ffmpeg tool to ffmpeg-next while init external fonts

--- src/types/config/mux.rs
+++ src/types/config/mux.rs
@@ -1,14 +1,18 @@
 use crate::{
     ArcPathBuf, CacheMIOfFile, CacheState, Config, EXTENSIONS, MediaInfo, Msg, MuxCurrent,
-    MuxError, Muxer, Result, Tool, Tools, i18n::logs, markers::MICmnStem,
+    MuxError, Muxer, Result,
+    ffmpeg::{self, sys},
+    i18n::logs,
+    markers::MICmnStem,
     types::input::iters::MediaGroupedByStem,
 };
 use log::{error, info, trace};
 use rayon::prelude::*;
 use std::{
-    ffi::OsString,
+    ffi::{CString, OsString},
     fs,
     path::{Path, PathBuf},
+    ptr,
     sync::Mutex,
 };
 
@@ -29,10 +33,10 @@ impl Config {
         let cnt = Mutex::new(0usize);
         let it = Mutex::new(self.input.iter_media_grouped_by_stem());
 
-        (0..self.jobs).into_par_iter().try_for_each(|t| {
-            let mut mi = MediaInfo::new(self, t);
+        (0..self.jobs).into_par_iter().try_for_each(|j| {
+            let mut mi = MediaInfo::new(self, j);
             loop {
-                let g = { it.lock().unwrap().next() };
+                let g = { it.lock().map_or(None, |mut it| it.next()) };
                 match g {
                     Some(g) => mux_media_group(self, fonts.as_ref(), &cnt, &mut mi, g)?,
                     None => return Ok::<(), MuxError>(()),
@@ -59,7 +63,7 @@ impl Config {
             };
 
             let out = cfg.output.temp_dir.join("external-fonts.mkv");
-            if let Err(e) = write_temp_fonts(cfg, fonts, &out) {
+            if let Err(e) = write_temp_fonts(fonts, &out) {
                 return fall(e);
             }
 
@@ -142,46 +146,105 @@ impl Config {
     }
 }
 
-fn write_temp_fonts(cfg: &Config, fonts: Vec<PathBuf>, out: &Path) -> Result<()> {
-    let srt = cfg.output.temp_dir.join("dummy-subs.srt");
+fn write_temp_fonts(fonts: Vec<PathBuf>, out: &Path) -> Result<()> {
+    let mut octx = ffmpeg::format::output(out)?;
+    add_dummy_subtitle_stream(&mut octx)?;
+    add_attachments(&mut octx, fonts);
 
-    {
-        let text = "1\n00:00:00,000 --> 00:00:01,000\n.\n";
-        fs::write(&srt, text)?;
+    octx.write_header()?;
+    write_dummy_subtitle_packet(&mut octx)?;
+    octx.write_trailer()?;
+    Ok(())
+}
+
+fn add_dummy_subtitle_stream(octx: &mut ffmpeg::format::context::Output) -> Result<()> {
+    unsafe {
+        let st = sys::avformat_new_stream(octx.as_mut_ptr(), ptr::null());
+        if st.is_null() {
+            return Err(err!("Fail add dummy subtitle stream"));
+        }
+
+        (*st).time_base = sys::AVRational { num: 1, den: 1000 };
+
+        let par = (*st).codecpar;
+        (*par).codec_type = sys::AVMediaType::AVMEDIA_TYPE_SUBTITLE;
+        (*par).codec_id = sys::AVCodecID::AV_CODEC_ID_SUBRIP;
     }
+    Ok(())
+}
 
-    let mut i = 0usize;
-    let metas: Vec<String> = fonts
-        .iter()
-        .map(|_| {
-            i += 1;
-            format!("-metadata:s:{}", i)
-        })
-        .collect();
+fn add_attachments(octx: &mut ffmpeg::format::context::Output, fonts: Vec<PathBuf>) {
+    for font in &fonts {
+        let name = some_or!(font.file_name(), continue);
+        let name = some_or!(CString::new(name.as_encoded_bytes()).ok(), continue);
 
-    let mut args = Vec::with_capacity(4 + fonts.len() * 4);
-    args.push(p!("-y"));
-    args.push(p!("-i"));
-    args.push(&srt);
-    fonts.iter().enumerate().for_each(|(i, f)| {
-        args.push(p!("-attach"));
-        args.push(f);
-        args.push(p!(&metas[i]));
-        args.push(p!(guess_mime(f)));
-    });
-    args.push(out);
+        let data = some_or!(fs::read(font).ok(), continue);
 
-    let tools = Tools::from(cfg);
-    let _ = tools.run(Tool::Ffmpeg, &args)?;
-    return Ok(());
+        let mime = match font.extension().map(|ext| ext.as_encoded_bytes()) {
+            Some(bs) if EXTENSIONS.otf.contains(bs) => c"application/vnd.ms-opentype",
+            _ => c"application/x-truetype-font",
+        };
 
-    fn guess_mime(f: &Path) -> &'static str {
-        if f.extension()
-            .map_or(false, |ext| EXTENSIONS.otf.contains(ext.as_encoded_bytes()))
-        {
-            "mimetype=application/vnd.ms-opentype"
-        } else {
-            "mimetype=application/x-truetype-font"
+        unsafe {
+            let size = data.len();
+            let buf = sys::av_malloc(size + sys::AV_INPUT_BUFFER_PADDING_SIZE as usize) as *mut u8;
+            if buf.is_null() {
+                continue;
+            }
+
+            let st = sys::avformat_new_stream(octx.as_mut_ptr(), ptr::null());
+            if st.is_null() {
+                sys::av_free(buf as *mut _);
+                continue;
+            }
+
+            let par = (*st).codecpar;
+            (*par).codec_type = sys::AVMediaType::AVMEDIA_TYPE_ATTACHMENT;
+            (*par).codec_id = sys::AVCodecID::AV_CODEC_ID_NONE;
+
+            ptr::copy_nonoverlapping(data.as_ptr(), buf, size);
+            (*par).extradata = buf;
+            (*par).extradata_size = size as i32;
+
+            sys::av_dict_set(&mut (*st).metadata, c"filename".as_ptr(), name.as_ptr(), 0);
+            sys::av_dict_set(&mut (*st).metadata, c"mimetype".as_ptr(), mime.as_ptr(), 0);
         }
     }
+}
+
+fn write_dummy_subtitle_packet(octx: &mut ffmpeg::format::context::Output) -> Result<()> {
+    let err = || Err(err!("Fail write dummy subtitle stream"));
+    let text = b"1\n00:00:00,000 --> 00:00:01,000\n.\n";
+
+    unsafe {
+        let buf = sys::av_malloc(text.len()) as *mut u8;
+        if buf.is_null() {
+            return err();
+        }
+
+        ptr::copy_nonoverlapping(text.as_ptr(), buf, text.len());
+
+        let pkt = sys::av_packet_alloc();
+        if pkt.is_null() {
+            sys::av_free(buf as *mut _);
+            return err();
+        }
+
+        sys::av_packet_from_data(pkt, buf, text.len() as i32);
+
+        (*pkt).stream_index = 0;
+        (*pkt).pts = 0;
+        (*pkt).dts = 0;
+        (*pkt).duration = 1000;
+
+        let ret = sys::av_interleaved_write_frame(octx.as_mut_ptr(), pkt);
+
+        sys::av_packet_unref(pkt);
+        sys::av_packet_free(&mut (pkt as *mut _));
+
+        if ret < 0 {
+            return err();
+        }
+    }
+    Ok(())
 }
